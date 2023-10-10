7d7d8ac341bbcd3c9f40a2994bf41fb08bf6bf9f
Fabian Kontor <fabiankontor@aol.com>
2023-10-10 16:37:02 +0200

    use codesplitter and treeindex

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import streamlit as st
 import os
 import sys
 import logging
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, get_response_synthesizer, load_index_from_storage, ServiceContext
-from llama_index.indices.document_summary import DocumentSummaryIndex, DocumentSummaryIndexRetriever
+from llama_index import TreeIndex, SimpleDirectoryReader, StorageContext, get_response_synthesizer, load_index_from_storage, ServiceContext
 from llama_index.llms import OpenAI
 from llama_index.query_engine import RetrieverQueryEngine
+from llama_index.text_splitter import CodeSplitter
+from llama_index.node_parser import SimpleNodeParser
 import openai
 import json
 import nest_asyncio
@@ -26,12 +27,6 @@ folder_path = sys.argv[-1] if len(sys.argv) > 1 else './results'
 # Get index name from folder name
 index_name = os.path.basename(os.path.normpath(folder_path))
 
-# Initialize session state
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {"role": "assistant", "content": f"Ask me a question about {index_name}'s document base!"}
-    ]
-
     
 @st.cache_resource(show_spinner=False)
 def load_data():
@@ -45,27 +40,22 @@ def load_data():
 		logging.info(f"Loaded index: {index}")
 		return index
 	except Exception as e:
-		SUMMARY_QUERY = (
-			"Describe what the provided text or code snippet is about. "
-			"You are working on a code project in dynamical systems reconstruction."
-			"Also describe some of the questions that can answered about the topic or project with this text."
-		)
-		
-		logging.info(f"Could not load index: {e}\nCreating index with summary query: {SUMMARY_QUERY}")
+		logging.info(f"Could not load index: {e}\nCreating new index")# with summary query: {SUMMARY_QUERY}")
 
 		documents = SimpleDirectoryReader(folder_path, recursive=True).load_data()
-		service_context = ServiceContext.from_defaults(llm=OpenAI(temperature=0, model="gpt-4"), chunk_size=2048)#, temperature=0.5, system_prompt="You are an expert on the LLama index Python library and your job is to answer technical questions. Assume that all questions are related to the LLama index Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+		service_context = ServiceContext.from_defaults(llm=OpenAI(temperature=0, model="gpt-4"))#, temperature=0.5, system_prompt="You are an expert on the LLama index Python library and your job is to answer technical questions. Assume that all questions are related to the LLama index Python library. Keep your answers technical and based on facts – do not hallucinate features."))
 
-		#index = VectorStoreIndex.from_documents(documents, service_context=service_context)
-		response_synthesizer = get_response_synthesizer(
-			response_mode="tree_summarize", use_async=True
+		text_splitter = CodeSplitter(
+		language="python",
+		chunk_lines=40,
+		chunk_lines_overlap=15,
+		max_chars=1500,
 		)
-		index = DocumentSummaryIndex.from_documents(
-			documents,
-			service_context=service_context,
-			response_synthesizer=response_synthesizer,
-            summary_query=SUMMARY_QUERY
-		)
+
+		node_parser = SimpleNodeParser.from_defaults(text_splitter=text_splitter)
+		service_context = ServiceContext.from_defaults(node_parser=node_parser)
+
+		index = TreeIndex.from_documents(documents, service_context=service_context)
 		index.set_index_id(index_name)
 		index.storage_context.persist(f"./storage/{index_name}")
 		logging.info(f"Created index: {index}")
@@ -73,54 +63,55 @@ def load_data():
 
 index = load_data()
 
+
 # Define chat engine
 #mode = st.selectbox("Select query mode", ["condense_question", "best", "context", "simple", "react", "openai"])
 #chat_engine = index.as_chat_engine(chat_mode=mode, verbose=True)
 
-response_mode = st.selectbox("Select response mode", ["refine", "compact", "tree_summarize", "simple_summarize", "no_text", "accumulate", "compact_accumulate"])
-
-choice_batch_size = st.number_input('Select the number of similar nodes to retrieve', min_value=1, max_value=100, value=10)
-
-#query_engine = index.as_query_engine(response_mode=response_mode)#, similarity_top_k=similarity_top_k, verbose=True)
-retriever = DocumentSummaryIndexRetriever(index, choice_batch_size=choice_batch_size, response_mode=response_mode, verbose=True)
-retriever.service_context = ServiceContext.from_defaults(llm=OpenAI(temperature=0, model="gpt-4"), chunk_size=1024)
-
-response_synthesizer = get_response_synthesizer()
+response_mode = st.selectbox("Select chat mode", ["best", "context", "condense_question", "simple", "react", "openai"])
 
 # assemble query engine
-query_engine = RetrieverQueryEngine(
-    retriever=retriever,
-    response_synthesizer=response_synthesizer
-)
+chat_engine = index.as_chat_engine(chat_mode=response_mode, verbose=True)
+
+st.subheader("Chat with {0} index".format(index_name))
+
+# Initialize session state
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = []
 
 # Chat UI
-if prompt := st.chat_input("Your question:"):
+if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+# Display chat
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
 # Perform and display the query
-if st.session_state.messages[-1]["role"] != "assistant":
+if len(st.session_state.messages) > 0 and st.session_state.messages[-1]["role"] != "assistant":
 	with st.chat_message("assistant"):
+		response = None
 		with st.spinner("Thinking..."):
-			#response = chat_engine.chat(prompt)
+			
 			logging.info(prompt)
-			response = query_engine.query(prompt)
-			#response = retriever.retrieve(prompt)
-			#print(response)
-			#for i, r in enumerate(response):
-			#	st.write(f"#{i+1}<br>{r.node.get_text()}")
-			#	st.session_state.messages.append({"role": "assistant", "content": r.node.get_text()})
-			# if is error
-			logging.warn(response)
-			st.write(f"{response.response}")
-			logging.info(response)
-			st.session_state.messages.append({"role": "assistant", "content": response.response})
+			# Initialize an empty string to hold the concatenated tokens
+			concatenated_response = ""
+
+			# Use stream_chat for streaming response
+			response = chat_engine.stream_chat(prompt)
+
+		message_placeholder = st.empty()
+		full_response = ""
+
+		for token in response.response_gen:
+			full_response += token
+			message_placeholder.write(full_response + "▌")
+		message_placeholder.write(full_response)
+		st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 # Button to export chat log
-if st.button('Export Chat Log'):
+if len(st.session_state.messages) > 0 and st.button('Export Chat Log'):
     chat_log_str = json.dumps(st.session_state.messages, indent=4)
     st.download_button(
         label="Download Chat Log",
